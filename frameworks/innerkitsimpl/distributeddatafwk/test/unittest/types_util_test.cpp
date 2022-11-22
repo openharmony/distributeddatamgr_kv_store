b494f5aad6d52c7170270d12587239d88836c255
丁冬冬 <dingdongdong4@huawei.com>
2022-11-22 03:55:22 +0000

    update frameworks/innerkitsimpl/distributeddatafwk/test/unittest/types_util_test.cpp.
    Signed-off-by: 丁冬冬 <dingdongdong4@huawei.com>

--- frameworks/innerkitsimpl/distributeddatafwk/test/unittest/types_util_test.cpp
+++ frameworks/innerkitsimpl/distributeddatafwk/test/unittest/types_util_test.cpp
@@ -22,42 +22,77 @@
 #include "iremote_proxy.h"
 #include "iremote_stub.h"
 #include "itypes_util.h"
+#include "rdb_types.h"
 #include "types.h"
 
 using namespace testing::ext;
-using namespace OHOS::DistributedKv;
 using namespace OHOS;
+using namespace OHOS::DistributedKv;
+using namespace OHOS::DistributedRdb;
+
 using var_t = std::variant<std::monostate, uint32_t, std::string, int32_t, uint64_t>;
+
 class TypesUtilTest : public testing::Test {
 public:
     class ITestRemoteObject : public IRemoteBroker {
     public:
         DECLARE_INTERFACE_DESCRIPTOR(u"OHOS.ITestRemoteObject");
     };
+
     class TestRemoteObjectStub : public IRemoteStub<ITestRemoteObject> {
     public:
     };
+
     class TestRemoteObjectProxy : public IRemoteProxy<ITestRemoteObject> {
     public:
         explicit TestRemoteObjectProxy(const sptr<IRemoteObject> &impl)
             : IRemoteProxy<ITestRemoteObject>(impl)
         {}
         ~TestRemoteObjectProxy() = default;
+
     private:
         static inline BrokerDelegator<TestRemoteObjectProxy> delegator_;
     };
+
     class TestRemoteObjectClient : public TestRemoteObjectStub {
     public:
         TestRemoteObjectClient() {}
         virtual ~TestRemoteObjectClient() {}
     };
+
     static void SetUpTestCase(void) {};
     static void TearDownTestCase(void) {};
     void SetUp() {};
     void TearDown() {};
 };
 
-HWTEST_F(TypesUtilTest, DeviceInfo, TestSize.Level0)
+/**
+* @tc.name: EntrySerialization
+* @tc.desc: Serialize entry
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, EntrySerialization, TestSize.Level1)
+{
+    MessageParcel parcel;
+    Entry entryIn;
+    entryIn.key = "student_name_mali";
+    entryIn.value = "age:20";
+    ASSERT_TRUE(ITypesUtil::Marshal(parcel, entryIn));
+
+    Entry entryOut;
+    ASSERT_TRUE(ITypesUtil::Unmarshal(parcel, entryOut));
+    EXPECT_EQ(entryOut.key.ToString(), std::string("student_name_mali"));
+    EXPECT_EQ(entryOut.value.ToString(), std::string("age:20"));
+}
+
+/**
+* @tc.name: DeviceInfoSerialization
+* @tc.desc: Serialize deviceInfo
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, DeviceInfoSerialization, TestSize.Level1)
 {
     MessageParcel parcel;
     DeviceInfo clientDev;
@@ -65,6 +100,7 @@ HWTEST_F(TypesUtilTest, DeviceInfo, TestSize.Level0)
     clientDev.deviceName = "rk3568";
     clientDev.deviceType = "phone";
     ASSERT_TRUE(ITypesUtil::Marshal(parcel, clientDev));
+
     DeviceInfo serverDev;
     ASSERT_TRUE(ITypesUtil::Unmarshal(parcel, serverDev));
     ASSERT_EQ(clientDev.deviceId, serverDev.deviceId);
@@ -72,20 +108,13 @@ HWTEST_F(TypesUtilTest, DeviceInfo, TestSize.Level0)
     ASSERT_EQ(clientDev.deviceType, serverDev.deviceType);
 }
 
-HWTEST_F(TypesUtilTest, Entry, TestSize.Level0)
-{
-    MessageParcel parcel;
-    Entry entryIn;
-    entryIn.key = "student_name_mali";
-    entryIn.value = "age:20";
-    ASSERT_TRUE(ITypesUtil::Marshal(parcel, entryIn));
-    Entry entryOut;
-    ASSERT_TRUE(ITypesUtil::Unmarshal(parcel, entryOut));
-    EXPECT_EQ(entryOut.key.ToString(), std::string("student_name_mali"));
-    EXPECT_EQ(entryOut.value.ToString(), std::string("age:20"));
-}
-
-HWTEST_F(TypesUtilTest, ChangeNotification, TestSize.Level1)
+/**
+* @tc.name: ChangeNotificationSerialization
+* @tc.desc: Serialize changeNotification
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, ChangeNotificationSerialization, TestSize.Level1)
 {
     Entry insert, update, del;
     insert.key = "insert";
@@ -98,10 +127,10 @@ HWTEST_F(TypesUtilTest, ChangeNotification, TestSize.Level1)
     inserts.push_back(insert);
     updates.push_back(update);
     deleteds.push_back(del);
-
     ChangeNotification changeIn(std::move(inserts), std::move(updates), std::move(deleteds), std::string(), false);
     MessageParcel parcel;
     ASSERT_TRUE(ITypesUtil::Marshal(parcel, changeIn));
+
     ChangeNotification changeOut({}, {}, {}, "", false);
     ASSERT_TRUE(ITypesUtil::Unmarshal(parcel, changeOut));
     ASSERT_EQ(changeOut.GetInsertEntries().size(), 1UL);
@@ -116,8 +145,93 @@ HWTEST_F(TypesUtilTest, ChangeNotification, TestSize.Level1)
     EXPECT_EQ(changeOut.IsClear(), false);
 }
 
+/**
+* @tc.name: SyncOptionSerialization
+* @tc.desc: Serialize syncOption
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, SyncOptionSerialization, TestSize.Level1)
+{
+    MessageParcel parcel;
+    SyncOption optionIn;
+    optionIn.mode = OHOS::DistributedRdb::SyncMode::PUSH;
+    optionIn.isBlock = true;
+    ASSERT_TRUE(ITypesUtil::Marshal(parcel, optionIn));
 
-HWTEST_F(TypesUtilTest, Multiple, TestSize.Level1)
+    SyncOption optionOut;
+    ASSERT_TRUE(ITypesUtil::Unmarshal(parcel, optionOut));
+    EXPECT_EQ(optionOut.mode, OHOS::DistributedRdb::SyncMode::PUSH);
+    EXPECT_EQ(optionOut.isBlock, true);
+}
+
+/**
+* @tc.name: RdbPredicatesSerialization
+* @tc.desc: Serialize rdbPredicates
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, RdbPredicatesSerialization, TestSize.Level1)
+{
+    MessageParcel parcel;
+    RdbPredicates predicatesIn;
+    predicatesIn.table_ = "table";
+    std::vector<std::string> devices;
+    devices.push_back("device");
+    predicatesIn.devices_ = devices;
+    std::vector<RdbPredicateOperation> operations;
+    RdbPredicateOperation operation;
+    operation.operator_ = RdbPredicateOperator::EQUAL_TO;
+    operation.field_ = "field";
+    std::vector<std::string> values;
+    values.push_back("value");
+    operation.values_ = values;
+    operations.push_back(operation);
+    predicatesIn.operations_ = operations;
+    ASSERT_TRUE(ITypesUtil::Marshal(parcel, predicatesIn));
+
+    RdbPredicates predicatesOut;
+    ASSERT_TRUE(ITypesUtil::Unmarshal(parcel, predicatesOut));
+    EXPECT_EQ(predicatesOut.table_, std::string("table"));
+    EXPECT_EQ(predicatesOut.devices_.size(), 1);
+    EXPECT_EQ(predicatesOut.devices_.front(), std::string("device"));
+    EXPECT_EQ(predicatesOut.operations_.size(), 1);
+    EXPECT_EQ(predicatesOut.operations_.front().operator_, RdbPredicateOperator::EQUAL_TO);
+    EXPECT_EQ(predicatesOut.operations_.front().field_, std::string("field"));
+    EXPECT_EQ(predicatesOut.operations_.front().values_.size(), 1);
+    EXPECT_EQ(predicatesOut.operations_.front().values_.front(), std::string("value"));
+}
+
+/**
+* @tc.name: OptionsSerialization
+* @tc.desc: Serialize options
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, OptionsSerialization, TestSize.Level1)
+{
+    MessageParcel parcel;
+    Options optionsIn;
+    optionsIn.schema = "schema";
+    optionsIn.hapName = "hapName";
+    ASSERT_TRUE(ITypesUtil::Marshal(parcel, optionsIn));
+
+    Options optionsOut;
+    ASSERT_TRUE(ITypesUtil::Unmarshal(parcel, optionsOut));
+    EXPECT_EQ(optionsOut.schema, std::string("schema"));
+    EXPECT_EQ(optionsOut.hapName, std::string("hapName"));
+    EXPECT_EQ(optionsOut.policies.size(), 1);
+    EXPECT_EQ(optionsOut.policies.front().type, PolicyType::IMMEDIATE_SYNC_ON_CHANGE);
+    EXPECT_EQ(optionsOut.policies.front().value.index(), 0);
+}
+
+/**
+* @tc.name: MultipleSerialization
+* @tc.desc: Serialize multiple
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, MultipleSerialization, TestSize.Level1)
 {
     uint32_t input1 = 10;
     int32_t input2 = -10;
@@ -130,6 +244,7 @@ HWTEST_F(TypesUtilTest, Multiple, TestSize.Level1)
     sptr<ITestRemoteObject> input7 = new TestRemoteObjectClient();
     MessageParcel parcel;
     ASSERT_TRUE(ITypesUtil::Marshal(parcel, input1, input2, input3, input4, input5, input6, input7->AsObject()));
+
     uint32_t output1 = 0;
     int32_t output2 = 0;
     std::string output3 = "";
@@ -150,7 +265,13 @@ HWTEST_F(TypesUtilTest, Multiple, TestSize.Level1)
     ASSERT_EQ(output7, input7->AsObject());
 }
 
-HWTEST_F(TypesUtilTest, Variant, TestSize.Level0)
+/**
+* @tc.name: VariantSerialization
+* @tc.desc: Serialize variant
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, VariantSerialization, TestSize.Level1)
 {
     MessageParcel parcelNull;
     var_t valueNullIn;
@@ -196,3 +317,32 @@ HWTEST_F(TypesUtilTest, Variant, TestSize.Level0)
     ASSERT_EQ(std::get<uint64_t>(valueUint64Out), 110);
 }
 
+/**
+* @tc.name: GetTotalSizeByEntries
+* @tc.desc: Get total size by entries
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, GetTotalSizeByEntries, TestSize.Level1)
+{
+    std::vector<Entry> entries;
+    Entry entry;
+    entry.key = "student_name_mali";
+    entry.value = "age:20";
+    entries.push_back(entry);
+    ASSERT_EQ(ITypesUtil::GetTotalSize(entries), sizeof(int) + std::string("student_name_mali").size() +
+                                                 sizeof(int) + std::string("age:20").size());
+}
+
+/**
+* @tc.name: GetTotalSizeByKeys
+* @tc.desc: Get total size by keys
+* @tc.type: FUNC
+* @tc.require: I61RH9
+*/
+HWTEST_F(TypesUtilTest, GetTotalSizeByKeys, TestSize.Level1)
+{
+    std::vector<Key> keys;
+    entries.push_back("student_name_mali");
+    ASSERT_EQ(ITypesUtil::GetTotalSize(keys), sizeof(int) + std::string("student_name_mali").size());
+}
