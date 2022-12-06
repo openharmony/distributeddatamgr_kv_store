727b9f0d91b1f56208a31123e78de846e05be756
wufengshan <wufengshan@huawei.com>
2022-12-06 16:25:14 +0800

    update
    Signed-off-by: wufengshan <wufengshan@huawei.com>

--- frameworks/innerkitsimpl/kvdb/test/single_store_impl_test.cpp
+++ frameworks/innerkitsimpl/kvdb/test/single_store_impl_test.cpp
@@ -24,6 +24,7 @@
 #include "types.h"
 using namespace testing::ext;
 using namespace OHOS::DistributedKv;
+namespace {
 class SingleStoreImplTest : public testing::Test {
 public:
     class TestObserver : public KvStoreObserver {
@@ -1291,4 +1292,5 @@ HWTEST_F(SingleStoreImplTest, GetKVStoreWithRebuildTrue, TestSize.Level0)
     ASSERT_NE(kvStore, nullptr);
     status = StoreManager::GetInstance().CloseKVStore(appId, storeId);
     ASSERT_EQ(status, SUCCESS);
+}
 }
