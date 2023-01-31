2b599211027441dd5c04a6c06d977f1538befd08
renjiecui <cuirenjie@huawei.com>
2023-01-31 17:26:36 +0800

    option添加是否为分布式数据库选项
    Signed-off-by: renjiecui <cuirenjie@huawei.com>

--- interfaces/innerkits/distributeddata/include/types.h
+++ interfaces/innerkits/distributeddata/include/types.h
@@ -235,7 +235,6 @@ struct Options {
     std::string schema = "";
     std::string hapName = "";
     std::string baseDir = "";
-    bool isDistributed = true;
 
     inline bool IsValidType() const
     {
