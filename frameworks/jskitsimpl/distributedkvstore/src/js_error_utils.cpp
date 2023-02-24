cd58cba3ec440edac18d955894518dffbc670d01
ylq121 <yangqing89@huawei.com>
2023-02-24 10:15:02 +0800

    xiugai2
    Signed-off-by: ylq121 <yangqing89@huawei.com>

--- frameworks/jskitsimpl/distributedkvstore/src/js_error_utils.cpp
+++ frameworks/jskitsimpl/distributedkvstore/src/js_error_utils.cpp
@@ -30,7 +30,7 @@ static constexpr JsErrorCode JS_ERROR_CODE_MSGS[] = {
     { Status::STORE_META_CHANGED, 15100002, "Open existed database with changed options." },
     { Status::PERMISSION_DENIED, 202, "Permission denied" },
     { Status::CRYPT_ERROR, 15100003, "Database corrupted." },
-    { Status::OVER_MAX_LIMITS, 15100001, "Over max subscribe limits." },
+    { Status::OVER_MAX_LIMITS, 15100001, "Over max  limits." },
     { Status::ALREADY_CLOSED, 15100005, "Database or result set already closed." },
     { Status::WAL_OVER_LIMITS, 15100006, "The wal file size over limit" },
 };
