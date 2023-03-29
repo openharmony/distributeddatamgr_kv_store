4437df8e30b7d9c424eb17cf25382d429c61f325
ylq121 <yangqing89@huawei.com>
2023-03-29 10:51:37 +0800

    delete duplicates
    Signed-off-by: ylq121 <yangqing89@huawei.com>

--- frameworks/innerkitsimpl/kvdb/include/kvdb_service.h
+++ frameworks/innerkitsimpl/kvdb/include/kvdb_service.h
@@ -63,8 +63,6 @@ public:
     virtual Status Unsubscribe(const AppId &appId, const StoreId &storeId, sptr<IKvStoreObserver> observer) = 0;
     virtual Status GetBackupPassword(
         const AppId &appId, const StoreId &storeId, std::vector<uint8_t> &password) = 0;
-    virtual DevBrief GetLocalDevice() = 0;
-    virtual std::vector<DevBrief> GetRemoteDevices() = 0;
 
 protected:
     enum TransId : int32_t {
@@ -87,8 +85,6 @@ protected:
         TRANS_UNSUB,
         TRANS_GET_PASSWORD,
         TRANS_NO_APPID_BEGIN,
-        TRANS_GET_LOCAL_DEVICE = TRANS_NO_APPID_BEGIN,
-        TRANS_GET_REMOTE_DEVICES,
         TRANS_NO_APPID_END,
         TRANS_BUTT = TRANS_NO_APPID_END,
     };
