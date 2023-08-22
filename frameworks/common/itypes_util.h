b27dbac18577e08f6abf0da38866ff3fed05948a
wangkai <wangkai424@huawei.com>
2023-08-22 11:08:12 +0800

    warning
    Signed-off-by: wangkai <wangkai424@huawei.com>

--- frameworks/common/itypes_util.h
+++ frameworks/common/itypes_util.h
@@ -397,7 +397,7 @@ bool ITypesUtil::UnmarshalFromContainer(T &val, MessageParcel &parcel)
 template<typename T>
 bool ITypesUtil::MarshalToBuffer(const T &input, int size, MessageParcel &data)
 {
-    if (size > MAX_SIZE || !data.WriteInt32(size)) {
+    if (size < 0 || static_cast<size_t>(size) > MAX_SIZE || !data.WriteInt32(size)) {
         return false;
     }
     if (size == 0) {
@@ -419,7 +419,7 @@ bool ITypesUtil::MarshalToBuffer(const T &input, int size, MessageParcel &data)
 template<typename T>
 bool ITypesUtil::MarshalToBuffer(const std::vector<T> &input, int size, MessageParcel &data)
 {
-    if (size > MAX_SIZE || input.size() > MAX_COUNT || !data.WriteInt32(size)) {
+    if (size < 0 || static_cast<size_t>(size) > MAX_SIZE || input.size() > MAX_COUNT || !data.WriteInt32(size)) {
         return false;
     }
     if (size == 0) {
@@ -451,7 +451,7 @@ bool ITypesUtil::UnmarshalFromBuffer(MessageParcel &data, T &output)
     if (size == 0) {
         return true;
     }
-    if (size > MAX_SIZE) {
+    if (size < 0 || static_cast<size_t>(size) > MAX_SIZE) {
         return false;
     }
     const uint8_t *buffer = reinterpret_cast<const uint8_t *>(data.ReadRawData(size));
@@ -468,12 +468,12 @@ bool ITypesUtil::UnmarshalFromBuffer(MessageParcel &data, std::vector<T> &output
     if (size == 0) {
         return true;
     }
-    if (size > MAX_SIZE) {
+    if (size < 0 || static_cast<size_t>(size) > MAX_SIZE) {
         return false;
     }
     int count = data.ReadInt32();
     const uint8_t *buffer = reinterpret_cast<const uint8_t *>(data.ReadRawData(size));
-    if (count < 0 || count > MAX_COUNT || buffer == nullptr) {
+    if (count < 0 || static_cast<size_t>(count) > MAX_COUNT || buffer == nullptr) {
         return false;
     }
 
