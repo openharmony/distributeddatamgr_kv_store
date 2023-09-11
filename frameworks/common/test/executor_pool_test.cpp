f02fe732ad47ef6b1cec7f74e5d3ccb42c5c3fb8
htt1997 <hutao105@huawei.com>
2023-09-11 19:11:05 +0800

    fix:fix remove
    Signed-off-by: htt1997 <hutao105@huawei.com>

--- frameworks/common/test/executor_pool_test.cpp
+++ frameworks/common/test/executor_pool_test.cpp
@@ -188,9 +188,8 @@ HWTEST_F(ExecutorPoolTest, MaxEqualsOne, TestSize.Level0)
     ASSERT_EQ(testNum, 4);
 }
 
-
 /**
-* @tc.name: RemoveWhenExcute
+* @tc.name: RemoveInExcuteTask
 * @tc.desc: test remove task when the task is running.
 * @tc.type: FUNC
 * @tc.require:
@@ -198,14 +197,18 @@ HWTEST_F(ExecutorPoolTest, MaxEqualsOne, TestSize.Level0)
 */
 HWTEST_F(ExecutorPoolTest, RemoveWhenExcute, TestSize.Level0)
 {
-    auto executors = std::make_shared<ExecutorPool>(2, 1);
+    auto executors = std::make_shared<ExecutorPool>(1, 1);
     auto taskId = ExecutorPool::INVALID_TASK_ID;
-    taskId = executors->Schedule(std::chrono::seconds(0), [executors, &taskId]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(SHORT_INTERVAL));
-        executors->Remove(taskId, false);
-        taskId = ExecutorPool::INVALID_TASK_ID;
-    });
+    std::atomic<int> flag = 0;
+    taskId = executors->Schedule(
+        [executors, &taskId, &flag]() {
+            flag++;
+            executors->Remove(taskId, false);
+            taskId = ExecutorPool::INVALID_TASK_ID;
+        },
+        std::chrono::seconds(0), std::chrono::milliseconds(SHORT_INTERVAL), 10);
     std::this_thread::sleep_for(std::chrono::seconds(LONG_INTERVAL));
     ASSERT_EQ(taskId, ExecutorPool::INVALID_TASK_ID);
+    ASSERT_EQ(flag, 1);
 }
 } // namespace OHOS::Test
