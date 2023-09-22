f364ebbe20c509d1ffc4c82f65537e432d2e0d8f
mazhao <mazhao12@huawei.com>
2023-09-22 19:38:14 +0800

    fix problem
    Signed-off-by: mazhao <mazhao12@huawei.com>
    Change-Id: Id637106ec0e1100c9d4bb482885046ab42e6821e

--- frameworks/libs/distributeddb/gaussdb_rd/build/CMakeFiles/document_ut.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for document_ut.
-# This may be replaced when dependencies are built.
--- frameworks/libs/distributeddb/gaussdb_rd/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mazhao/distributeddatamgr_datamgr_service_new/services/distributeddataservice/service/data_share/gaussdb_rd
-# Build directory: /home/mazhao/distributeddatamgr_datamgr_service_new/services/distributeddataservice/service/data_share/gaussdb_rd/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
