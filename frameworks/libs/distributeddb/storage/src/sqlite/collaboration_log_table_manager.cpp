78595a9f0660c3e31464bba6b6f5975a93785509
lobty <butaoyu@huawei.com>
2023-10-07 17:04:39 +0800

    Fix compatibility issues with distributed table ut
    Signed-off-by: lobty <butaoyu@huawei.com>

--- frameworks/libs/distributeddb/storage/src/sqlite/collaboration_log_table_manager.cpp
+++ frameworks/libs/distributeddb/storage/src/sqlite/collaboration_log_table_manager.cpp
@@ -90,7 +90,7 @@ std::string CollaborationLogTableManager::GetUpdateTrigger(const TableInfo &tabl
             std::string(DBConstant::SQLITE_INNER_ROWID) + ", '', '', get_sys_time(0), "
             "get_last_time(), CASE WHEN (" + CalcPrimaryKeyHash("NEW.", table, identity) + " != " +
             CalcPrimaryKeyHash("NEW.", table, identity) + ") THEN 0x02 ELSE 0x22 END, " +
-            CalcPrimaryKeyHash("NEW.", table, identity) + ", '');\n";
+            CalcPrimaryKeyHash("NEW.", table, identity) + ", '', '', '');\n";
     }
     updateTrigger += "END;";
     return updateTrigger;
