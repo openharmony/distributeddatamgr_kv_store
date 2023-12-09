8ae0c24be9cc65a3f630193ba10b2ec9592dcc66
zqq <zhangqiquan1@huawei.com>
2023-12-09 12:00:58 +0800

    fix issue
    Signed-off-by: zqq <zhangqiquan1@huawei.com>

--- frameworks/libs/distributeddb/communicator/src/communicator_aggregator.cpp
+++ frameworks/libs/distributeddb/communicator/src/communicator_aggregator.cpp
@@ -396,7 +396,7 @@ void CommunicatorAggregator::SendPacketsAndDisposeTask(const SendTask &inTask, u
         RetrySendTaskIfNeed(inTask.dstTarget);
     }
     if (taskNeedFinalize) {
-        TaskFinalizer(inTask, !inTask.isValid ? -E_PERIPHERAL_INTERFACE_FAIL : errCode);
+        TaskFinalizer(inTask, errCode);
         std::lock_guard<std::mutex> autoLock(sendRecordMutex_);
         sendRecord_.erase(inTask.frameId);
     }
