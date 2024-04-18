bda66b07e9fd7433c40728f01e4eeb491edabe41
yangliu <yangliu178@huawei.com>
2024-04-18 19:54:01 +0800

    update
    Signed-off-by: yangliu <yangliu178@huawei.com>

--- frameworks/jskitsimpl/distributedkvstore/src/js_field_node.cpp
+++ frameworks/jskitsimpl/distributedkvstore/src/js_field_node.cpp
@@ -113,7 +113,7 @@ napi_value JsFieldNode::AppendChild(napi_env env, napi_callback_info info)
         ASSERT_BUSINESS_ERR(ctxt, argc >= 1, Status::INVALID_ARGUMENT, "Parameter error:Mandatory parameters are left unspecified");
         ctxt->status = JSUtil::Unwrap(env, argv[0], reinterpret_cast<void**>(&child), JsFieldNode::Constructor(env));
         ASSERT_BUSINESS_ERR(ctxt, ((ctxt->status == napi_ok) && (child != nullptr)),
-            Status::INVALID_ARGUMENT, "Parameter error:Parameters verification failed");
+            Status::INVALID_ARGUMENT, "Parameter error:child nullptr");
     };
     ctxt->GetCbInfoSync(env, info, input);
     ASSERT_NULL(!ctxt->isThrowError, "AppendChild exit");
