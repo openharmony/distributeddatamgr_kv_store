29f18650b5ad552f104f07db866391e73498a66e
zqq <zhangqiquan1@huawei.com>
2024-04-27 19:01:31 +0800

    format gn
    Signed-off-by: zqq <zhangqiquan1@huawei.com>

--- frameworks/libs/distributeddb/interfaces/include/cloud/cloud_store_types.h
+++ frameworks/libs/distributeddb/interfaces/include/cloud/cloud_store_types.h
@@ -147,9 +147,9 @@ struct CloudSyncOption {
     bool priorityTask = false;
     bool compensatedSyncOnly = false;
     std::vector<std::string> users;
+    bool merge = false;
     // default, upload insert need lock
     LockAction lockAction = LockAction::INSERT;
-    bool merge = false;
 };
 
 enum class QueryNodeType : uint32_t {
