71582d485f6bf50c1daac5632ec2a4ddbefed59a
zqq <zhangqiquan1@huawei.com>
2024-06-11 09:35:57 +0800

    encode and decode device with base64
    Signed-off-by: zqq <zhangqiquan1@huawei.com>

---
+++ frameworks/libs/distributeddb/common/include/db_base64_utils.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DB_BASE64_UTILS_H
+#define DB_BASE64_UTILS_H
+
+#include <string>
+#include <vector>
+
+namespace DistributedDB {
+class DBBase64Utils {
+public:
+    static std::string Encode(const std::vector<uint8_t> &source);
+    static std::vector<uint8_t> Decode(const std::string &encoded);
+};
+}
+#endif // DB_BASE64_UTILS_H
