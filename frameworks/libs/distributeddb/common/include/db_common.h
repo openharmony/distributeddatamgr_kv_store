72cb3f23f68361f06f57d4e86040ef209175d541
MarkoLF <liufuchenxing1@huawei.com>
2024-08-21 11:43:20 +0800

    Revert "support delete file monitor"
    This reverts commit ef6b4da0f3178b42bbe3baebe12facd1f77fbd78.
    
    Signed-off-by: MarkoLF <liufuchenxing1@huawei.com>

--- frameworks/libs/distributeddb/common/include/db_common.h
+++ frameworks/libs/distributeddb/common/include/db_common.h
@@ -27,10 +27,6 @@
 namespace DistributedDB {
 class DBCommon final {
 public:
-    enum FileControlType {
-        SET_FLAG,
-        CLEAR_FLAG
-    };
     static int CreateDirectory(const std::string &directory);
 
     static void StringToVector(const std::string &src, std::vector<uint8_t> &dst);
@@ -150,8 +146,6 @@ public:
     static std::string GetCursorKey(const std::string &tableName);
 
     static void RemoveDuplicateAssetsData(std::vector<Asset> &assets);
-
-    static void SetOrClearFSMonitorFlag(const std::string &fileName, FileControlType fileControlType);
 private:
     static void InsertNodesByScore(const std::map<std::string, std::map<std::string, bool>> &graph,
         const std::vector<std::string> &generateNodes, const std::map<std::string, int> &scoreGraph,
