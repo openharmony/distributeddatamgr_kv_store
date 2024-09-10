d26e435e4c9c1bac3b1ab0043e1048f5249e2b9a
HuangHe <huanghe78@huawei.com>
2024-09-10 15:32:52 +0800

    同步gaussdbrd分支差异
    Signed-off-by: HuangHe <huanghe78@huawei.com>

--- frameworks/libs/distributeddb/gaussdb_rd/src/common/include/grd_api_manager.h
+++ frameworks/libs/distributeddb/gaussdb_rd/src/common/include/grd_api_manager.h
@@ -88,5 +88,6 @@ struct GRD_APIInfo {
     KVBatchDestory KVBatchDestoryApi = nullptr;
 };
 GRD_APIInfo GetApiInfoInstance();
+void InitApiInfo(const char *configStr);
 } // namespace DocumentDB
 #endif // __cplusplus
