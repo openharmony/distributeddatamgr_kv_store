9ee294a0babebdab65714c3a245238ee6142fd53
ding_dong_dong <dingdongdong4@huawei.com>
2024-09-29 19:48:25 +0800

    modify code
    Signed-off-by: ding_dong_dong <dingdongdong4@huawei.com>

--- frameworks/innerkitsimpl/kvdb/src/store_manager.cpp
+++ frameworks/innerkitsimpl/kvdb/src/store_manager.cpp
@@ -55,11 +55,12 @@ std::shared_ptr<SingleKvStore> StoreManager::GetKVStore(const AppId &appId, cons
 
     bool isCreate = false;
     auto kvStore = StoreFactory::GetInstance().GetOrOpenStore(appId, storeId, options, status, isCreate);
-    if (status == CRYPT_ERROR) {
-        ZLOGW("crypto error or corrupted database,storeId:%{public}s", StoreUtil::Anonymous(storeId.storeId).c_str());
+    if (status == DATA_CORRUPTED) {
+        ZLOGW("database is corrupt, storeId:%{public}s", StoreUtil::Anonymous(storeId.storeId).c_str());
         KvStoreTuple tuple = { .appId = appId.appId, .storeId = storeId.storeId };
         auto repoterDir = KVDBFaultHiViewReporter::GetDBPath(path, storeId.storeId);
         KVDBFaultHiViewReporter::ReportKVDBCorruptedFault(options, status, errno, tuple, repoterDir);
+        status = CRYPT_ERROR;
     }
     if (options.rebuild && status == SUCCESS) {
         ZLOGI("rebuild store success, storeId:%{public}s", StoreUtil::Anonymous(storeId.storeId).c_str());
