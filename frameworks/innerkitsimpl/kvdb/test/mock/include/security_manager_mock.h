d9802657dd6461f64f722bea5fd4afb601c54858
caozhijun <caozhijun12@huawei.com>
2024-11-09 17:18:49 +0800

    add ut to cover branches
    Signed-off-by: c459517356 <caozhijun12@huawei.com>

---
+++ frameworks/innerkitsimpl/kvdb/test/mock/include/security_manager_mock.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_DISTRIBUTED_DATA_SECURITY_MANAGER_MOCK_H
+#define OHOS_DISTRIBUTED_DATA_SECURITY_MANAGER_MOCK_H
+
+#include <gmock/gmock.h>
+#include "security_manager.h"
+
+namespace OHOS::DistributedKv {
+class BSecurityManager {
+public:
+    virtual bool SaveDBPassword(const std::string &, const std::string &, const DistributedDB::CipherPassword &) = 0;
+    virtual SecurityManager::DBPassword GetDBPassword(const std::string &, const std::string &, bool) = 0;
+    BSecurityManager() = default;
+    virtual ~BSecurityManager() = default;
+public:
+    static inline std::shared_ptr<BSecurityManager> securityManager = nullptr;
+};
+
+class SecurityManagerMock : public BSecurityManager {
+public:
+    MOCK_METHOD(bool, SaveDBPassword, (const std::string&, const std::string&, const DistributedDB::CipherPassword&));
+    MOCK_METHOD(SecurityManager::DBPassword, GetDBPassword, (const std::string&, const std::string&, bool));
+};
+} // namespace OHOS::DistributedKv
+#endif // OHOS_DISTRIBUTED_DATA_STORE_UTIL_MOCK_H
---
+++ frameworks/innerkitsimpl/kvdb/test/mock/src/store_util_mock.cpp
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "include/store_util_mock.h"
+
+namespace OHOS::DistributedKv {
+
+bool StoreUtil::IsFileExist(const std::string &name)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return false;
+    }
+    return BStoreUtil::storeUtil->IsFileExist(name);
+}
+
+bool StoreUtil::Remove(const std::string &path)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return false;
+    }
+    return BStoreUtil::storeUtil->Remove(path);
+}
+
+Status StoreUtil::ConvertStatus(StoreUtil::DBStatus status)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return Status::ERROR;
+    }
+    return BStoreUtil::storeUtil->ConvertStatus(status);
+}
+
+StoreUtil::DBSecurity StoreUtil::GetDBSecurity(int32_t secLevel)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return { DistributedDB::NOT_SET, DistributedDB::ECE };
+    }
+    return BStoreUtil::storeUtil->GetDBSecurity(secLevel);
+}
+
+bool StoreUtil::InitPath(const std::string &path)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return false;
+    }
+    return BStoreUtil::storeUtil->InitPath(path);
+}
+
+StoreUtil::DBIndexType StoreUtil::GetDBIndexType(IndexType type)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return DistributedDB::HASH;
+    }
+    return BStoreUtil::storeUtil->GetDBIndexType(type);
+}
+
+std::vector<std::string> StoreUtil::GetSubPath(const std::string &path)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        std::vector<std::string> vec;
+        return vec;
+    }
+    return BStoreUtil::storeUtil->GetSubPath(path);
+}
+
+std::vector<StoreUtil::FileInfo> StoreUtil::GetFiles(const std::string &path)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        std::vector<StoreUtil::FileInfo> fileInfos;
+        return fileInfos;
+    }
+    return BStoreUtil::storeUtil->GetFiles(path);
+}
+
+bool StoreUtil::Rename(const std::string &oldName, const std::string &newName)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return false;
+    }
+    return BStoreUtil::storeUtil->Rename(oldName, newName);
+}
+
+std::string StoreUtil::Anonymous(const std::string &name)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return "";
+    }
+    return BStoreUtil::storeUtil->Anonymous(name);
+}
+
+uint32_t StoreUtil::Anonymous(const void *ptr)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return 0;
+    }
+    return BStoreUtil::storeUtil->Anonymous(ptr);
+}
+
+bool StoreUtil::RemoveRWXForOthers(const std::string &path)
+{
+    if (BStoreUtil::storeUtil == nullptr) {
+        return false;
+    }
+    return BStoreUtil::storeUtil->RemoveRWXForOthers(path);
+}
+} // namespace OHOS::DistributedKv
