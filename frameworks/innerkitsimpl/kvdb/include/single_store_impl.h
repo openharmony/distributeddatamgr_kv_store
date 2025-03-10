ff79415ef368e6bccba905e05af7cbc80eaac347
yanhui <yanhui35@huawei.com>
2025-03-10 18:06:22 +0800

    API版本隔离改至20
    Signed-off-by: yanhui <yanhui35@huawei.com>
    Change-Id: I9192edb951fa8a9fa21bf82caeb1a541e944b0ea

--- frameworks/innerkitsimpl/kvdb/include/single_store_impl.h
+++ frameworks/innerkitsimpl/kvdb/include/single_store_impl.h
@@ -117,7 +117,7 @@ private:
             (std::chrono::steady_clock::now() + std::chrono::milliseconds(offset)).time_since_epoch())
             .count();
     }
-    static constexpr int32_t INTEGRITY_CHECK_API_VERSION = 18;
+    static constexpr int32_t INTEGRITY_CHECK_API_VERSION = 20;
     static constexpr uint32_t NOTIFY_INTERVAL = 200; // ms
     static constexpr size_t MAX_VALUE_LENGTH = 4 * 1024 * 1024;
     static constexpr size_t MAX_OBSERVER_SIZE = 8;
