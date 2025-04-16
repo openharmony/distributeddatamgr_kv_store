49063abdc5271976026930638d705d9eb6e78548
谭凯圣 <tankaisheng@huawei.com>
2025-04-16 09:39:27 +0800

    【分布式DB】修复存量数据的表进行设置关联关系和端云同步并发锁占用超时的问题
    Signed-off-by: 谭凯圣 <tankaisheng@huawei.com>

--- frameworks/libs/distributeddb/storage/src/sqlite/relational/sqlite_relational_store.cpp
+++ frameworks/libs/distributeddb/storage/src/sqlite/relational/sqlite_relational_store.cpp
@@ -1403,12 +1403,6 @@ int SQLiteRelationalStore::CleanWaterMark(SQLiteSingleVerRelationalStorageExecut
             return errCode;
         }
     }
-#ifdef USE_DISTRIBUTEDDB_CLOUD
-    errCode = cloudSyncer_->CleanWaterMarkInMemory(clearWaterMarkTable);
-    if (errCode != E_OK) {
-        LOGE("[SQLiteRelationalStore] CleanWaterMarkInMemory failed, errCode = %d", errCode);
-    }
-#endif
     return errCode;
 }
 
@@ -1446,12 +1440,21 @@ int SQLiteRelationalStore::SetReference(const std::vector<TableReferenceProperty
 
     int ret = handle->Commit();
     ReleaseHandle(handle);
-    if (ret == E_OK) {
-        sqliteStorageEngine_->SetSchema(schema);
-        return errCode;
+    if (ret != E_OK) {
+        LOGE("[SQLiteRelationalStore] SetReference commit transaction failed, errCode = %d", ret);
+        return ret;
     }
-    LOGE("[SQLiteRelationalStore] SetReference commit transaction failed, errCode = %d", ret);
-    return ret;
+    sqliteStorageEngine_->SetSchema(schema);
+#ifdef USE_DISTRIBUTEDDB_CLOUD
+    if (!clearWaterMarkTables.empty()) {
+        ret = cloudSyncer_->CleanWaterMarkInMemory(clearWaterMarkTables);
+        if (ret != E_OK) {
+            LOGE("[SQLiteRelationalStore] CleanWaterMarkInMemory failed, errCode = %d", errCode);
+            return ret;
+        }
+    }
+#endif
+    return errCode;
 }
 
 int SQLiteRelationalStore::InitTrackerSchemaFromMeta()
