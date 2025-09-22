128e8ee0d45eea01fb901da62612334124f6e1e5
bigteer <zhangdi126@huawei.com>
2025-09-22 22:37:57 +0800

    跨平台问题解决
    Signed-off-by: bigteer <zhangdi126@huawei.com>

--- kvstoremock/interfaces/mock/common/napi_base_context.h
+++ kvstoremock/interfaces/mock/common/napi_base_context.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,26 +12,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 #ifndef ABILITY_RUNTIME_NAPI_BASE_CONTEXT_H
 #define ABILITY_RUNTIME_NAPI_BASE_CONTEXT_H
-
+ 
 #include <memory>
-
+ 
 #include "napi/native_api.h"
-
+ 
 namespace OHOS {
 namespace AppExecFwk {
 class Ability;
 }
 namespace AbilityRuntime {
 napi_value* GetFAModeContextClassObject();
-
+ 
 napi_status IsStageContext(napi_env env, napi_value object, bool& stageMode);
-
+ 
 class Context;
 std::shared_ptr<Context> GetStageModeContext(napi_env env, napi_value object);
-
+ 
 AppExecFwk::Ability* GetCurrentAbility(napi_env env);
 }  // namespace AbilityRuntime
 }  // namespace OHOS
