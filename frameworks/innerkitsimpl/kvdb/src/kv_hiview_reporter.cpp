93fa3e20b996d3b6c979e79e31bdbdeffe906447
zhanglele33 <zhanglele33@h-partners.com>
2025-12-15 11:46:08 +0800

    修复上传明文路径
    Signed-off-by: zhanglele33 <zhanglele33@h-partners.com>

--- frameworks/innerkitsimpl/kvdb/src/kv_hiview_reporter.cpp
+++ frameworks/innerkitsimpl/kvdb/src/kv_hiview_reporter.cpp
@@ -334,6 +334,6 @@ std::string KVDBFaultHiViewReporter::GenerateAppendix(const KVDBFaultEvent &even
     fileInfo += std::string(SHM_NAME) + ":" + GetFileStatInfo(eventInfo.dbPath + SHM_PATH) + "\n";
     fileInfo += std::string(WAL_NAME) + ":" + GetFileStatInfo(eventInfo.dbPath + WAL_PATH) + "\n";
     fileInfo += std::string(KEY_NAME) + ":" + GetFileStatInfo(eventInfo.keyPath);
-    return FUNCTION + eventInfo.functionName + "\n" + DBPATH + eventInfo.dbPath + "\n" + FILEINFO + fileInfo;
+    return FUNCTION + eventInfo.functionName + "\n" + DBPATH + StoreUtil::Anonymous(eventInfo.dbPath).c_str() + "\n" + FILEINFO + fileInfo;
 }
 } // namespace OHOS::DistributedKv
