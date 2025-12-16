07f57a765641d1723fa70289cd95636eb0f71724
zhanglele33 <zhanglele33@h-partners.com>
2025-12-16 10:21:09 +0800

    修改测试用例
    Signed-off-by: zhanglele33 <zhanglele33@h-partners.com>

--- frameworks/innerkitsimpl/kvdb/test/kv_hiview_reporter_test.cpp
+++ frameworks/innerkitsimpl/kvdb/test/kv_hiview_reporter_test.cpp
@@ -29,7 +29,8 @@ using namespace testing::ext;
 using namespace OHOS::DistributedKv;
 
 static constexpr const char *BASE_DIR = "/data/service/el1/public/database/KvHiviewReporterTest/";
-static constexpr const char *STOREID = "test_storeId";
+static constexpr const char *STOREID = "SingleKVStore";
+static constexpr const char *APPID = "KvHiviewReporterTest";
 static constexpr const char *DB_CORRUPTED_POSTFIX = ".corruptedflg";
 static constexpr const char *FULL_KVDB_PATH = "/data/service/el1/public/database/KvHiviewReporterTest/kvdb/";
 static constexpr const char *FULL_KEY_PATH = "/data/service/el1/public/database/KvHiviewReporterTest/key/";
@@ -43,6 +44,7 @@ public:
 
     void SetUp();
     void TearDown();
+    std::shared_ptr<SingleKvStore> kvStore_;
 };
 
 void KvHiviewReporterTest::SetUpTestCase(void)
@@ -51,35 +53,44 @@ void KvHiviewReporterTest::SetUpTestCase(void)
     if (ret != 0) {
         ZLOGE("Mkdir failed, result:%{public}d, path:%{public}s", ret, BASE_DIR);
     }
+    ASSERT_EQ(ret, 0);
+    kvStore_ = CreateKVStore(STOREID, SINGLE_VERSION, true, false);
+    ASSERT_NE(kvStore_, nullptr);
+
 }
 
 void KvHiviewReporterTest::TearDownTestCase(void)
-{
-    auto ret = remove(BASE_DIR);
-    if (ret != 0) {
-        ZLOGE("Remove failed, result:%{public}d, path:%{public}s", ret, BASE_DIR);
-    }
+
+    kvStore_ = nullptr;
+    AppId appId = { APPID } ;
+    StoreId storeId = { STOREID };
+    auto baseDir = BASE_DIR;
+    auto status = StoreManager::GetInstance().Delete(appId, storeId, baseDir);
+    ASSERT_EQ(status, SUCCESS);
+    (void)remove(FULL_KVDB_PATH);
+    (void)remove(FULL_KEY_PATH);
+    (void)remove(BASE_DIR);
+
 }
 
 void KvHiviewReporterTest::SetUp(void) { }
 
 void KvHiviewReporterTest::TearDown(void) { }
 
-/**
- * @tc.name: CorruptedFlagTest001
- * @tc.desc: Create and delete corrupted flag
- * @tc.type: FUNC
- */
-HWTEST_F(KvHiviewReporterTest, CorruptedFlagTest001, TestSize.Level1)
+std::shared_ptr<SingleKvStore> KvHiviewReporterTest::CreateKVStore(
+    std::string storeIdTest, KvStoreType type, bool encrypt, bool backup)
 {
-    ZLOGI("CorruptedFlagTest001 begin.");
-    KVDBFaultHiViewReporter::CreateCorruptedFlag(BASE_DIR, STOREID);
-    std::string flagFilename = std::string(BASE_DIR) + std::string(STOREID) + std::string(DB_CORRUPTED_POSTFIX);
-    auto ret = access(flagFilename.c_str(), F_OK);
-    ASSERT_EQ(ret, 0);
-    KVDBFaultHiViewReporter::DeleteCorruptedFlag(BASE_DIR, STOREID);
-    ret = access(flagFilename.c_str(), F_OK);
-    ASSERT_NE(ret, 0);
+    Options options;
+    options.kvStoreType = type;
+    options.securityLevel = S1;
+    options.encrypt = encrypt;
+    options.area = EL1;
+    options.backup = backup;
+    options.baseDir = BASE_DIR;
+
+    AppId appId = { "KvHiviewReporterTest" };
+    StoreId storeId = { storeIdTest };
+    return StoreManager::GetInstance().GetKVStore(appId, storeId, options, status);
 }
 
 /**
@@ -87,58 +98,47 @@ HWTEST_F(KvHiviewReporterTest, CorruptedFlagTest001, TestSize.Level1)
  * @tc.desc: Execute the ReportFaultEvent method
  * @tc.type: FUNC
  */
-HWTEST_F(KvHiviewReporterTest, ReportKVFaultEvent001, TestSize.Level1)
+HWTEST_F(KvHiviewReporterTest, ReportKVFaultEvent001, TestSize.Level0)
 {
     ZLOGI("ReportKVFaultEvent001 begin.");
-    HiSysEventMock mock;
-    EXPECT_CALL(mock, HiSysEvent_Write(_, _, _, _, _, _, _)).Times(1);
-    ReportInfo reportInfo;
-    KVDBFaultHiViewReporter::ReportKVFaultEvent(reportInfo);
-}
-
-/**
- * @tc.name: ReportKVFaultEvent002
- * @tc.desc: Execute the ReportFaultEvent method
- * @tc.type: FUNC
- */
-HWTEST_F(KvHiviewReporterTest, ReportKVFaultEvent002, TestSize.Level1)
-{
-    ZLOGI("ReportKVFaultEvent002 getKvStore begin.");
-    std::shared_ptr<SingleKvStore> kvStore;
-    AppId appId = { "KvHiviewReporterTest" };
-    StoreId storeId = { "SingleKVStore" };
+    AppId appId = { APPID } ;
+    StoreId storeId = { STOREID };
     Options options;
-    options.kvStoreType = SINGLE_VERSION;
-    options.securityLevel = S1;
-    options.area = EL1;
     options.baseDir = BASE_DIR;
-    options.backup = false;
     options.encrypt = true;
-    Status status;
-    kvStore = StoreManager::GetInstance().GetKVStore(appId, storeId, options, status);
-    ASSERT_NE(kvStore, nullptr);
-
-    ZLOGI("ReportKVFaultEvent002 reportKVFaultEvent begin.");
+    Status status = DATA_CORRUPTED;;
     HiSysEventMock mock;
-    EXPECT_CALL(mock, HiSysEvent_Write(_, _, _, _, _, _, _)).Times(3);
-    status = DATA_CORRUPTED;
+    EXPECT_CALL(mock, HiSysEvent_Write(_, _, _, _, _, _, _)).Times(2);
     ReportInfo reportInfo = { .options = options, .errorCode = status, .systemErrorNo = errno,
                 .appId = appId.appId, .storeId = storeId.storeId, .functionName = std::string(__FUNCTION__) };
     KVDBFaultHiViewReporter::ReportKVFaultEvent(reportInfo);
+    std::string flagFilename = std::string(BASE_DIR) + std::string(STOREID) + std::string(DB_CORRUPTED_POSTFIX);
+    auto ret = access(flagFilename.c_str(), F_OK);
+    ASSERT_NE(ret, 0);
+}
 
-    ZLOGI("ReportKVFaultEvent002 delete kvStore begin.");
-    std::string baseDir = BASE_DIR;
-    StoreManager::GetInstance().Delete(appId, storeId, baseDir);
-    ZLOGI("ReportKVFaultEvent002 delete kvStore end.");
-
-    ZLOGI("ReportKVFaultEvent002 delete dir begin.");
+/**
+ * @tc.name: ReportKVRebuildEvent001
+ * @tc.desc: Execute the ReportKVRebuildEvent method
+ * @tc.type: FUNC
+ */
+HWTEST_F(KvHiviewReporterTest, ReportKVRebuildEvent001, TestSize.Level0)
+{
+    ZLOGI("ReportKVRebuildEvent001 begin.");
+    AppId appId = { APPID } ;
+    StoreId storeId = { STOREID };
+    Options options;
+    options.baseDir = BASE_DIR;
+    options.encrypt = true;
+    Status status = SUCCESS;
+    HiSysEventMock mock;
+    EXPECT_CALL(mock, HiSysEvent_Write(_, _, _, _, _, _, _)).Times(1);
+    ReportInfo reportInfo = { .options = options, .errorCode = status, .systemErrorNo = errno,
+                .appId = APPID, .storeId = STOREID, .functionName = std::string(__FUNCTION__) };
+    KVDBFaultHiViewReporter::ReportKVFaultEvent(reportInfo);
     std::string dbPath = KVDBFaultHiViewReporter::GetDBPath(options.GetDatabaseDir(), storeId.storeId);
     auto ret = remove(dbPath.c_str());
     ASSERT_EQ(ret, 0);
-    ret = remove(FULL_KVDB_PATH);
-    ASSERT_EQ(ret, 0);
-    ret = remove(FULL_KEY_PATH);
-    ASSERT_EQ(ret, 0);
-    ZLOGI("ReportKVFaultEvent002 delete dir end.");
+
 }
 } // namespace OHOS::Test
