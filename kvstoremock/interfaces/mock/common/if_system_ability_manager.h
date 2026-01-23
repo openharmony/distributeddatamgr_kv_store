6c6cfc463b3b44556c6e0e1ecbb59d778d765882
quguiren <quguiren1@huawei-partners.com>
2026-01-23 10:43:51 +0800

    kv_store支持跨平台
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

---
+++ kvstoremock/interfaces/mock/common/if_system_ability_manager.h
@@ -0,0 +1,30 @@
+/*
+* Copyright (c) 2026 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#ifndef DISTRIBUTED_KVSTORE_MOCK_IF_SYSTEM_ABILITY_MANAGER_H
+#define DISTRIBUTED_KVSTORE_MOCK_IF_SYSTEM_ABILITY_MANAGER_H
+#include "iremote_broker.h"
+
+namespace OHOS {
+
+class ISystemAbilityManager : public IRemoteBroker {
+public:
+    ISystemAbilityManager() = default;
+    sptr<IRemoteObject> CheckSystemAbility(int32_t systemAbilityId)
+    {
+        return nullptr;
+    }
+};
+}  // namespace OHOS
+#endif  // DISTRIBUTED_KVSTORE_MOCK_IF_SYSTEM_ABILITY_MANAGER_H
