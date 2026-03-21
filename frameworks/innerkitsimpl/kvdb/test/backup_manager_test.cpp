3fda0245d98dc3985b81cae4afc800cb04e0acab
z30053452 <zhiyihang1@huawei.com>
2026-03-21 21:52:40 +0800

    code fix
    Signed-off-by: z30053452 <zhiyihang1@huawei.com>

--- frameworks/innerkitsimpl/kvdb/test/backup_manager_test.cpp
+++ frameworks/innerkitsimpl/kvdb/test/backup_manager_test.cpp
@@ -652,8 +652,8 @@ HWTEST_F(BackupManagerTest, RestoreEncryptWithKeyFromService, TestSize.Level0)
     ASSERT_EQ(status, SUCCESS);
     status = StoreManager::GetInstance().Delete(appId, storeId, baseDir);
     ASSERT_EQ(status, SUCCESS);
-    AppId appId = { "" };
-    StoreId storeId = { "" };
+    appId = { "" };
+    storeId = { "" };
     status = StoreManager::GetInstance().Delete(appId, storeId, baseDir);
     ASSERT_EQ(status, INVALID_ARGUMENT);
 }
